04e9b7dfc110c9aab314231472871a6e260876e6
qubingquan <qubingquan1@100tal.com>
2021-02-20 16:11:51 +0800

    4

--- src/main/java/com/quan/redistest/service/TestService.java
+++ src/main/java/com/quan/redistest/service/TestService.java
@@ -77,6 +77,11 @@ public class TestService {
     }
 
     public int add2(int a,int b){
+        StringBuilder sb = new StringBuilder();
+        sb.append("1");
+        sb.append("1");
+        sb.append("1");
+        sb.append("1");
         return a+b;
     }
 
